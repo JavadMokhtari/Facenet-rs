74b35899ff20056c8fd2ea5ab7a4e5f59d7f4e9f
Javad-Mokhtari <javad.mktri@gmail.com>
2026-02-05 11:23:43 +0330

    - Changed detect method to accept single DynamicImage - Changed return type to Vec<FaceBox> - Fixed other issues in code

--- src/detection.rs
+++ src/detection.rs
@@ -1,12 +1,12 @@
 use std::path::Path;
 
-use crate::configs;
+use crate::configs::FaceDetectorConfig;
 use image::{DynamicImage, GenericImageView, imageops::FilterType};
 use ndarray::{Array, Axis, Dim, Ix, s};
 use ort::session::Session;
 use ort::value::TensorRef;
 
-use crate::models::BoundingBox;
+use crate::models::FaceBox;
 use crate::utils::{intersection, union};
 
 pub struct YOLOFaceDetector {
@@ -19,7 +19,7 @@ impl YOLOFaceDetector {
     pub fn new() -> YOLOFaceDetector {
         let session = Session::builder()
             .unwrap()
-            .commit_from_file(configs::YOLO_MODEL_PATH)
+            .commit_from_file(FaceDetectorConfig::MODEL_PATH)
             .unwrap();
         Self {
             model: session,
@@ -54,94 +54,90 @@ impl YOLOFaceDetector {
         input
     }
 
-    pub fn detect(&mut self, images: &[DynamicImage]) -> Vec<Vec<(BoundingBox, f32)>> {
-        let mut arrays = Vec::with_capacity(images.len());
-        for image in images {
-            let input_tensor = self.preprocess(image);
-            // arrays.push(input.view());
-            let input = ort::inputs![&self.input_name => TensorRef::from_array_view(&input_tensor).unwrap()];
-            let outputs = self.model.run(input).unwrap();
-            let output = outputs
-                .get(&self.output_name.as_str())
-                .unwrap()
-                .try_extract_array::<f32>()
-                .unwrap()
-                .t()
-                .into_owned();
-            // }
-            // let batch_tensor = ndarray::concatenate(Axis(0), &arrays).unwrap();
-            //
-            // // ndarray concatenate returns ArrayxD, we need Array4
-            // let batch_tensor = batch_tensor.into_dimensionality::<ndarray::Ix4>().unwrap();
-            //
-            // // Ensure input is contiguous in memory (CowArray)
-            // let input_contiguous = batch_tensor.as_standard_layout();
-            //
-            // // Create input tensor reference from ndarray view
-            // let input_tensor = TensorRef::from_array_view(input_contiguous.view()).unwrap();
-            //
-            // // Run session - inputs! macro returns a Vec, not a Result
-            // let inputs = ort::inputs![&self.input_name => input_tensor];
-            //
-            // let outputs = self.model.run(inputs).unwrap();
-            //
-            // let output = outputs
-            //     .get(&self.output_name.as_str())
-            //     .unwrap()
-            //     .try_extract_array::<f32>()?
-            //     .t()
-            //     .into_owned();
+    pub fn detect(&mut self, image: &DynamicImage) -> Vec<FaceBox> {
+        let input_tensor = self.preprocess(image);
+        // arrays.push(input.view());
+        let input =
+            ort::inputs![&self.input_name => TensorRef::from_array_view(&input_tensor).unwrap()];
+        let outputs = self.model.run(input).unwrap();
+        let output = outputs
+            .get(&self.output_name.as_str())
+            .unwrap()
+            .try_extract_array::<f32>()
+            .unwrap()
+            .t()
+            .into_owned();
+        // }
+        // let batch_tensor = ndarray::concatenate(Axis(0), &arrays).unwrap();
+        //
+        // // ndarray concatenate returns ArrayxD, we need Array4
+        // let batch_tensor = batch_tensor.into_dimensionality::<ndarray::Ix4>().unwrap();
+        //
+        // // Ensure input is contiguous in memory (CowArray)
+        // let input_contiguous = batch_tensor.as_standard_layout();
+        //
+        // // Create input tensor reference from ndarray view
+        // let input_tensor = TensorRef::from_array_view(input_contiguous.view()).unwrap();
+        //
+        // // Run session - inputs! macro returns a Vec, not a Result
+        // let inputs = ort::inputs![&self.input_name => input_tensor];
+        //
+        // let outputs = self.model.run(inputs).unwrap();
+        //
+        // let output = outputs
+        //     .get(&self.output_name.as_str())
+        //     .unwrap()
+        //     .try_extract_array::<f32>()?
+        //     .t()
+        //     .into_owned();
 
-            let mut boxes = Vec::new();
-            let output = output.slice(s![.., .., 0]);
-            let (img_width, img_height) = (image.width(), image.height());
-            for row in output.axis_iter(Axis(0)) {
-                let row: Vec<_> = row.iter().copied().collect();
-                let (_, prob) = row
-                    .iter()
-                    // skip bounding box coordinates
-                    .skip(4)
-                    .enumerate()
-                    .map(|(index, value)| (index, *value))
-                    .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
-                    .unwrap();
-                if prob < configs::FACE_DETECTION_CONFIDENCE_THRESHOLD {
-                    continue;
-                }
-                let xc = row[0] / 640. * (img_width as f32);
-                let yc = row[1] / 640. * (img_height as f32);
-                let w = row[2] / 640. * (img_width as f32);
-                let h = row[3] / 640. * (img_height as f32);
-                boxes.push((
-                    BoundingBox {
-                        x1: xc - w / 2.,
-                        y1: yc - h / 2.,
-                        x2: xc + w / 2.,
-                        y2: yc + h / 2.,
-                    },
-                    prob,
-                ));
+        let mut boxes = Vec::new();
+        let output = output.slice(s![.., .., 0]);
+        let (img_width, img_height) = (image.width(), image.height());
+        for row in output.axis_iter(Axis(0)) {
+            let row: Vec<_> = row.iter().copied().collect();
+            let (_, prob) = row
+                .iter()
+                // skip bounding box coordinates
+                .skip(4)
+                .enumerate()
+                .map(|(index, value)| (index, *value))
+                .reduce(|accum, row| if row.1 > accum.1 { row } else { accum })
+                .unwrap();
+            if prob < FaceDetectorConfig::CONFIDENCE {
+                continue;
             }
-
-            boxes.sort_by(|box1, box2| box2.1.total_cmp(&box1.1));
-            let mut result = Vec::new();
-
-            while !boxes.is_empty() {
-                result.push(boxes[0]);
-                boxes = boxes
-                    .iter()
-                    .filter(|box1| {
-                        intersection(&boxes[0].0, &box1.0) / union(&boxes[0].0, &box1.0) < 0.7
-                    })
-                    .copied()
-                    .collect();
-            }
-            arrays.push(result);
+            let xc = row[0] / 640. * (img_width as f32);
+            let yc = row[1] / 640. * (img_height as f32);
+            let w = row[2] / 640. * (img_width as f32);
+            let h = row[3] / 640. * (img_height as f32);
+            boxes.push(FaceBox {
+                x1: xc - w / 2.,
+                y1: yc - h / 2.,
+                x2: xc + w / 2.,
+                y2: yc + h / 2.,
+                confidence: prob,
+            });
         }
-        arrays
+
+        boxes.sort_by(|box1, box2| box2.confidence.total_cmp(&box1.confidence));
+        let mut result = Vec::new();
+
+        while !boxes.is_empty() {
+            result.push(boxes[0]);
+            boxes = boxes
+                .iter()
+                .filter(|box1| {
+                    intersection(&boxes[0], &box1) / union(&boxes[0], &box1)
+                        < FaceDetectorConfig::IOU
+                })
+                .copied()
+                .collect();
+        }
+        result
     }
 }
-//
+
 //         let results = self.model.run(image).unwrap();
 //         let mut faces = Vec::new();
 //
