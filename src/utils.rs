4582a7b9125741e72af30f411e31daada80774db
Javad-Mokhtari <javad.mktri@gmail.com>
2026-02-05 16:37:10 +0330

    Fixed FaceBox name in code

--- src/utils.rs
+++ src/utils.rs
@@ -2,7 +2,7 @@ use std::path::Path;
 
 use image::DynamicImage;
 
-use crate::models::BoundingBox;
+use crate::models::FaceBox;
 
 pub fn read_image(src: &str) -> DynamicImage {
     // Load image using standard image crate
@@ -10,11 +10,11 @@ pub fn read_image(src: &str) -> DynamicImage {
     img
 }
 
-pub fn intersection(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+pub fn intersection(box1: &FaceBox, box2: &FaceBox) -> f32 {
     (box1.x2.min(box2.x2) - box1.x1.max(box2.x1)) * (box1.y2.min(box2.y2) - box1.y1.max(box2.y1))
 }
 
-pub fn union(box1: &BoundingBox, box2: &BoundingBox) -> f32 {
+pub fn union(box1: &FaceBox, box2: &FaceBox) -> f32 {
     ((box1.x2 - box1.x1) * (box1.y2 - box1.y1)) + ((box2.x2 - box2.x1) * (box2.y2 - box2.y1))
         - intersection(box1, box2)
 }
