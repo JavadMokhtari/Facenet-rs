d5cc35a1c08ed08a97093e2fb62f07ac45391bd7
Javad-Mokhtari <javad.mktri@gmail.com>
2026-02-05 23:00:03 +0330

    Fixed issues in code

--- example/src/main.rs
+++ example/src/main.rs
@@ -1,11 +1,12 @@
-use facenet::{detect_face, extract_embedding};
-
-const IMAGE_PATH: &str = "../assets/cool_girl.jpg";
+use facenet::extract_embedding;
 
 fn main() {
+    let image_path = "../assets/cool_girl.jpg".to_string();
+
     let now = std::time::Instant::now();
-    let result = extract_embedding(IMAGE_PATH);
+    let result = extract_embedding(image_path);
     let elapsed = now.elapsed();
-    println!("{:?}", result);
-    println!("Elapsed: {:?}", elapsed);
+
+    println!("Face embedding length: {:?}", result.len());
+    println!("Process time: {:?}", elapsed);
 }
--- src/lib.rs
+++ src/lib.rs
@@ -5,25 +5,28 @@ mod recognition;
 mod utils;
 
 use detection::YOLOFaceDetector;
-use models::FaceBox;
 use recognition::FaceNetModel;
 use utils::read_image;
 
 uniffi::setup_scaffolding!();
 
-pub fn extract_embedding(image_path: &str) -> Vec<f32> {
-    let mut detector = YOLOFaceDetector::new();
-    let image = read_image(image_path);
+#[uniffi::export]
+pub fn extract_embedding(image_path: String) -> Vec<f32> {
+    let mut detector = YOLOFaceDetector::init();
+    let image = read_image(&image_path);
     let output = detector.detect(&image);
     let face = output[0];
+    println!("Detected face box {:?}", face);
 
-    let mut facenet = FaceNetModel::new();
+    let mut facenet = FaceNetModel::init();
+    let now = std::time::Instant::now();
     let embedding = facenet.extract_embedding(&image, face);
+    println!("Feature extraction time taken: {:?}", now.elapsed());
     embedding
 }
 
 #[uniffi::export]
-pub fn detect_face(image_path: &str) -> Vec<FaceBox> {
+pub fn detect_face(image_path: String) -> Vec<Vec<f32>> {
     // let mut arrays = Vec::with_capacity(images.len());
     // for image in images {
     //     let result = detector.detect(&image);
@@ -31,8 +34,12 @@ pub fn detect_face(image_path: &str) -> Vec<FaceBox> {
     // }
     // arrays
 
-    let mut detector = YOLOFaceDetector::new();
-    let image = read_image(image_path);
+    let mut detector = YOLOFaceDetector::init();
+    let image = read_image(&image_path);
     let output = detector.detect(&image);
-    output
+    let mut faces = Vec::new();
+    for face in output {
+        faces.push(vec![face.x1, face.y1, face.x2, face.y2, face.confidence]);
+    }
+    faces
 }
