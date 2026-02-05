c1a3599f99baad3b36c7b85c852b6d452cd3ab4d
Javad-Mokhtari <javad.mktri@gmail.com>
2026-02-05 16:39:47 +0330

    Edited BoundingBox to FaceBox and added confidence field

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,8 @@
-#[derive(Debug, Clone, Copy)]
-pub struct BoundingBox {
+#[derive(uniffi::Record, Debug, Clone, Copy)]
+pub struct FaceBox {
     pub x1: f32,
     pub y1: f32,
     pub x2: f32,
-    pub y2: f32
+    pub y2: f32,
+    pub confidence: f32,
 }
