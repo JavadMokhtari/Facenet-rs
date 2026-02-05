3b48f95a23a0ad5fd518f8343ba7a476c799b501
Javad-Mokhtari <javad.mktri@gmail.com>
2026-02-05 16:38:43 +0330

    Wrote config structs for FaceDetector and Facenet but not completed

--- src/configs.rs
+++ src/configs.rs
@@ -1,4 +1,15 @@
-pub const YOLO_MODEL_PATH: &str = "../assets/yolov11s-face.onnx";
-pub const FACENET_MODEL_PATH: &str = "../assets/facenet128.onnx";
+pub struct FaceDetectorConfig;
 
-pub const FACE_DETECTION_CONFIDENCE_THRESHOLD: f32 = 0.5;
+impl FaceDetectorConfig {
+    pub const MODEL_PATH: &str = "../assets/yolov11s-face.onnx";
+    pub const INPUT_SIZE: u32 = 320;
+    pub const CONFIDENCE: f32 = 0.5;
+    pub const IOU: f32 = 0.5;
+
+}
+
+pub struct FacenetConfig;
+
+impl FacenetConfig {
+    pub const MODEL_PATH: &str = "../assets/facenet128.onnx";
+}
