6366be94b511d77bc3e6f4741794b7dd4ce06750
j.mokhtari <j.mokhtari@matiran.ir>
2026-02-14 05:18:36 -0800

    Added detect_face_from_dir function to lib

--- src/lib.rs
+++ src/lib.rs
@@ -4,29 +4,35 @@ mod models;
 mod recognition;
 mod utils;
 
+use crate::detection::yolo_inference;
+use crate::models::FaceBox;
 use detection::YOLOFaceDetector;
 use recognition::FaceNetModel;
+use std::path::Path;
+use std::time::Instant;
 use utils::read_image;
 
 uniffi::setup_scaffolding!();
 
 #[uniffi::export]
-pub fn extract_embedding(image_path: String) -> Vec<f32> {
+pub fn extract_embedding(image_path: &str) -> Vec<f32> {
+    let now = Instant::now();
     let mut detector = YOLOFaceDetector::init();
     let image = read_image(&image_path);
     let output = detector.detect(&image);
     let face = output[0];
     println!("Detected face box {:?}", face);
 
+    println!("Face detection time: {:?}", now.elapsed());
+
     let mut facenet = FaceNetModel::init();
-    let now = std::time::Instant::now();
     let embedding = facenet.extract_embedding(&image, face);
-    println!("Feature extraction time taken: {:?}", now.elapsed());
+    println!("Feature extraction time: {:?}", now.elapsed());
     embedding
 }
 
 #[uniffi::export]
-pub fn detect_face(image_path: String) -> Vec<Vec<f32>> {
+pub fn detect_face(image_path: &str) -> Vec<Vec<f32>> {
     // let mut arrays = Vec::with_capacity(images.len());
     // for image in images {
     //     let result = detector.detect(&image);
@@ -42,4 +48,11 @@ pub fn detect_face(image_path: String) -> Vec<Vec<f32>> {
         faces.push(vec![face.x1, face.y1, face.x2, face.y2, face.confidence]);
     }
     faces
+}
+
+#[uniffi::export]
+pub fn detect_face_from_dir(imgs_dir: &str) -> Vec<Vec<FaceBox>> {
+    let dir = Path::new(imgs_dir);
+    let outputs = yolo_inference(dir);
+    outputs
 }
